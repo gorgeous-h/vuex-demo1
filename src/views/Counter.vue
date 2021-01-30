bafcc7596008d1baabf42e4b3def85946576526a
hejiajun <847455348@qq.com>
2021-01-30 12:34:07 +0800

    Vuex核心概念之State

--- src/views/Counter.vue
+++ src/views/Counter.vue
@@ -1,16 +1,43 @@
 <template>
     <div>
         <button @click="increment">加一</button> {{count}}
+        <!--<br/>-->
+        <!--{{countAlias}}-->
+        <!--<br/>-->
+        <!--{{countPlusLocalState}}-->
+        <br/>
+        {{localComputed}}
     </div>
 </template>
 
 <script>
+    import {mapState} from 'vuex'
+
     export default {
         name: "Counter",
-        computed: {
-            count() {
-                return this.$store.state.count
+        data() {
+            return {
+                localCount: 2
             }
+        },
+        // computed: {
+        //     count() {
+        //         return this.$store.state.count
+        //     }
+        // },
+        // computed: mapState({
+        //     count: state => state.count,
+        //     countAlias: 'count',
+        //     countPlusLocalState(state) {
+        //         return state.count + this.localCount
+        //     },
+        // }),
+        // computed: mapState(['count']), // 这里通过名称匹配Vuex实例中的state数据
+        computed: {
+            localComputed () {
+                return this.count + this.localCount
+            },
+            ...mapState(['count']),
         },
         methods: {
             increment() {
