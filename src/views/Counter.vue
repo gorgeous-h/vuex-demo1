ac8ff8d726dcf8cdade7d2a08f70e8654843a7e4
hejiajun <847455348@qq.com>
2021-01-30 10:41:54 +0800

    Vuex入门

---
+++ src/views/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <button @click="increment">加一</button> {{count}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Counter",
+        computed: {
+            count() {
+                return this.$store.state.count
+            }
+        },
+        methods: {
+            increment() {
+                this.$store.commit('increment')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
